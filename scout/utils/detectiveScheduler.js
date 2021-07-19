3cccd1aafe49849a7348b78ef38fce56911a667b
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2021-07-19 11:13:04 +0300

    The scheduler now unschedules a node once it stops responding instead of crashing the sever.

--- scout/utils/detectiveScheduler.js
+++ scout/utils/detectiveScheduler.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const timestamper = require('./timestamper')
 
 const schedulePerpetually = function(subscriber) {
-    let responseCode
+    let continueSubscription = false
 
-    do { // <------------------- Make sure there's always a break case
+    do {
         const timestamp = timestamper.getTimestamp()
         const cameraMacAddress = "12:a3:4b:56:78:c9"
         let hmPeopleDetected
@@ -17,28 +17,29 @@ const schedulePerpetually = function(subscriber) {
             'headers': {
             },
             formData: {
-            'image': {
-                'value': fs.createReadStream('/home/ttzvetkov/Downloads/intruders.jpg'),
-                'options': {
-                'filename': 'intruders.jpg',
-                'contentType': null
+                'image': {
+                    'value': fs.createReadStream('/home/ttzvetkov/Downloads/intruders.jpg'),
+                    'options': {
+                    'filename': 'intruders.jpg',
+                    'contentType': null
+                    }
                 }
-            }
             }
         }
 
         request(options, function (error, response) {
-            if (error) throw new Error(error)
-
-            responseCode = response.statusCode
+            if (error) {
+                console.log("Subscription of detective node " + subscriber + " terminated with error: " + error)
+                continueSubscription = false // <------------- Breaks subscription
+                return
+            }
 
             hmPeopleDetected = response.body
             console.log("People detected: " + hmPeopleDetected + " at: " + timestamp + " from camera with MAC: " + cameraMacAddress)
 
             schedulePerpetually(subscriber)
         })
-    } while (responseCode === 200)
-    
+    } while (continueSubscription)
 }
 
 exports.schedulePerpetually = schedulePerpetually
