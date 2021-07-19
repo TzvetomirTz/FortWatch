8b9ab2b4edf43416bc71de89d3a33d927f3be710
TzvetomirTz <cvetomiriv.cvetkov@gmail.com>
2021-07-19 10:57:57 +0300

    The scheduler no longer uses infinite recursion to perpetuate the requests to the subscribed nodes.

--- scout/utils/detectiveScheduler.js
+++ scout/utils/detectiveScheduler.js
@@ -4,38 +4,41 @@ const fs = require('fs');
 const timestamper = require('./timestamper')
 
 const schedulePerpetually = function(subscriber) {
-    const timestamp = timestamper.getTimestamp()
-    const cameraMacAddress = "12:a3:4b:56:78:c9"
-    let hmPeopleDetected
+    let responseCode
 
-    const options = {
-        'method': 'POST',
-        'url': 'http://localhost:5000/check/',
-        'headers': {
-        },
-        formData: {
-          'image': {
-            'value': fs.createReadStream('/home/ttzvetkov/Downloads/intruders.jpg'),
-            'options': {
-              'filename': 'intruders.jpg',
-              'contentType': null
+    do { // <------------------- Make sure there's always a break case
+        const timestamp = timestamper.getTimestamp()
+        const cameraMacAddress = "12:a3:4b:56:78:c9"
+        let hmPeopleDetected
+
+        const options = {
+            'method': 'POST',
+            'url': 'http://localhost:5000/check/',
+            'headers': {
+            },
+            formData: {
+            'image': {
+                'value': fs.createReadStream('/home/ttzvetkov/Downloads/intruders.jpg'),
+                'options': {
+                'filename': 'intruders.jpg',
+                'contentType': null
+                }
             }
-          }
+            }
         }
-      }
 
-      request(options, function (error, response) {
-        if (error) throw new Error(error)
+        request(options, function (error, response) {
+            if (error) throw new Error(error)
 
-        hmPeopleDetected = response.body
-        console.log("People detected: " + hmPeopleDetected + " at: " + timestamp + " from camera with MAC: " + cameraMacAddress)
+            responseCode = response.statusCode
 
-        // const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-        // await delay(1000) /// waiting 1 second.
+            hmPeopleDetected = response.body
+            console.log("People detected: " + hmPeopleDetected + " at: " + timestamp + " from camera with MAC: " + cameraMacAddress)
 
-        schedulePerpetually(subscriber) // Again and again as soon as the node is ready to take a new sample.
-        //Recursion in js should be avoided. So in the future replace with an infinite loop that breaks on lack of response from the node.
-      })
+            schedulePerpetually(subscriber)
+        })
+    } while (responseCode === 200)
+    
 }
 
 exports.schedulePerpetually = schedulePerpetually
